033c535de1fbdb40937f436f6373f88e198998d2
ZIA <68376285+SnakesterZ@users.noreply.github.com>
2020-09-02 20:53:25 +0530

    Add files via upload

---
+++ Tree.js
@@ -0,0 +1,27 @@
+class Tree{
+  constructor(x,y,w,h){
+    var option ={
+      
+      isStatic:true
+    }
+    
+      this.body = Bodies.rectangle(x, y, w, h, option);
+      this.width = w;
+      this.height = h;
+      this.image = loadImage('tree.png')
+      
+      World.add(world, this.body);
+      
+  }
+    
+  display(){
+    var pos = this.body.position;
+    var angle = this.body.angle;
+    
+   
+   imageMode(CENTER)
+    image(this.image, pos.x, pos.y, this.width, this.height)
+   
+    
+  }
+}
